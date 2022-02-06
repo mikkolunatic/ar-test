7090ec116385394c92d93c0495756fd276d44ebe
Mikael Bedard <bedard.mikael@gmail.com>
2022-02-06 14:10:25 -0500

    Test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var marker1 = { lat: 46.84750141414454, lng: -71.21877981084529 };
+var marker1 = [46.84750141414454, -71.21877981084529];
 var user = null;
 var x = document.getElementById('demo');
 var r = document.getElementById('data');
@@ -18,19 +18,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var current_position, current_accuracy;
 
+var blueDot = L.icon({
+    iconUrl: './assets/blue-dot.png',
+
+    iconSize: [25, 25], // size of the icon
+});
+var markerUnknow = L.icon({
+    iconUrl: './assets/marker-unknow.png',
+
+    iconSize: [21, 30], // size of the icon
+});
+
+L.marker(marker1, {icon: markerUnknow}).addTo(map);
+
 function onLocationFound(e) {
     var radius = e.accuracy;
 
     if (current_position) {
-      map.removeLayer(current_position);
-      map.removeLayer(current_accuracy);
+      current_position.setLatLng(e.latlng);
+      current_accuracy.setLatLng(e.latlng);
     }else{
       //Set the view to position if it is the first time
       map.setView(e.latlng, 19);
+      current_position = L.marker(e.latlng, {icon: blueDot}).addTo(map);
+      current_accuracy = L.circle(e.latlng, radius).addTo(map);
     }
-
-    current_position = L.marker(e.latlng).addTo(map);
-    current_accuracy = L.circle(e.latlng, radius).addTo(map);
 }
 
 map.on('locationfound', onLocationFound);
